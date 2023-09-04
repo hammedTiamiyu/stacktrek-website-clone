8d3edcfdefaa44389642b0c0147ed52d6a268a66
hammedTiamiyu <hammed.tiamiyu@grandiasolutions.com>
2023-09-04 11:18:04 +0100

    router added

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ErrorPage = () => {
+    return (
+        <>
+            <h1>Error</h1>
+            <h3>Page not found! Please ensure the url address is correct;/</h3>
+            <h3> Check of your internet connection is stable</h3>
+        </>
+    )
+}
+
+export default ErrorPage
